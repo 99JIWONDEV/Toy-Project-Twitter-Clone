2b1f3ac7075b9ec7e52814086a87bc908208a1fe
99JIWONDEV <99jiwonpark@gmail.com>
2024-03-10 21:29:06 +0900

    firebase 설치 및 기초 setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Layout from "./components/layout"
-import Home from "./routes/home"
-import Profile from "./routes/profile"
-import Login from "./routes/login"
-import CreateAccount from "./routes/create-account"
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Layout/>,
+    path: "/",
+    element: <Layout />,
     // children을 사용해서 중첩 사용 가능 (layout도 뜨면서, children도 뜸)
     children: [
       {
-        path:"",
-        element:<Home/>
+        path: "",
+        element: <Home />,
       },
       {
-        path:"profile",
-        element:<Profile/>
-      }
-    ]
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
   },
   {
-    path:'/login',
-    element: <Login />
+    path: "/login",
+    element: <Login />,
   },
   {
-    path:'/create-account',
-    element:<CreateAccount/>
-  }
-])
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
@@ -45,14 +46,23 @@ const GlobalStyles = createGlobalStyle`
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
   }
-`
+`;
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // 파이어베이스 기다리기
+    setLoading(false)
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <>
-    <GlobalStyles/>
-    <RouterProvider router={router} />
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbW0QS0q80pipnOf0936Q1-gKSTQVPsOI",
+  authDomain: "twitter-clone-13a2b.firebaseapp.com",
+  projectId: "twitter-clone-13a2b",
+  storageBucket: "twitter-clone-13a2b.appspot.com",
+  messagingSenderId: "534563905852",
+  appId: "1:534563905852:web:30fe12e73f8865db683e39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

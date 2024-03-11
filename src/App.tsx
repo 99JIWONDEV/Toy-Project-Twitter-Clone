76b38e5f8e7a92715c02d178c8f5a356d9b3cec4
99JIWONDEV <99jiwonpark@gmail.com>
2024-03-11 21:13:51 +0900

    로그인 모듈 개발

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,38 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, styled } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
     // children을 사용해서 중첩 사용 가능 (layout도 뜨면서, children도 뜸)
     children: [
       {
         path: "",
-        element: <Home />,
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "profile",
-        element: <Profile />,
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
       },
     ],
   },
@@ -47,21 +61,29 @@ const GlobalStyles = createGlobalStyle`
 
   }
 `;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
     // 파이어베이스 기다리기
-    setLoading(false)
+    await auth.authStateReady();
+    setLoading(false);
   };
   useEffect(() => {
     init();
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { Error, Form, Input, Password, Switcher, Title, Wrapper } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
+
 export default function Login() {
-  return(
-    <h1>Login</h1>
-  )
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('')
+
+    if (isLoading || email==="" || password ==="") return;
+    try {
+      setLoading(true)
+      await signInWithEmailAndPassword(auth, email, password)
+      navigate('/')
+    } catch (e) {
+      //setError
+      if (e instanceof FirebaseError){
+        setError(e.message)
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  const passwordReset = () => {
+    sendPasswordResetEmail(auth, email)
+      try{
+        {alert("비밀번호 변경 메일을 보내드렸습니다.")}
+      }catch(e){
+        setError("등록되지 않은 사용자입니다.")
+      }
+  }
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+        <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+        <Input onChange={onChange} type="submit" value={isLoading ? "Loading..." : "Login"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        계정이 없으신가요?{" "} <Link to="/create-account">회원가입 &rarr;</Link>
+      </Switcher>
+      <Password onClick={passwordReset}>
+        비밀번호를 잊으셨나요?
+      </Password>
+      <GithubButton/>
+    </Wrapper>
+  );
 }
